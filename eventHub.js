d7eda436cd4e92371004ea7eec1d6121f0f731a9
Jaap Mengers <jaapm@q42.nl>
2016-06-07 17:07:26 +0200

    Update eventHub.js

--- eventHub.js
+++ eventHub.js
@@ -1,54 +1,25 @@
 
 module.exports = function(RED) {
     function AzureEventHub(config) {
-        var request = require('request');
-        var saToken = require('./saToken.js');
-        
         RED.nodes.createNode(this,config);
-        var node = this;
 
-        this.endpoint = config.endpoint;
-        this.endpointConfig = RED.nodes.getNode(this.endpoint);
+        var eventHubs = require('eventhubs-js');
 
-        this.status({fill:"red",shape:"ring",text:"waiting"});
-
-        if (this.endpointConfig) {
-            this.on('input', function(msg) {
-
-                //var body = '{ "device": "laptop", "message": "My message body." }';
-                var body = '{ "temp":"' + msg +'"}';
-                
-                var namespace = config.namespace;
-                var hubName = config.hubname;
-                var saName = config.saname;
-                var saKey = config.sakey;
-                var token = saToken.create(namespace, hubName, saName, saKey);
-                var serviceBusUriSuffix = '?timeout=60&api-version=2014-01';
-
-                var uri = 'https://' + namespace + '.servicebus.windows.net/' +
-                    hubName + '/messages' + serviceBusUriSuffix;
-                this.status({fill:"green",shape:"dot",text:"sending"});
-                request({
-                'uri': uri,
-                'method': 'POST',
-                'headers': {
-                    'Content-Type' : 'application/atom+xml;type=entry;charset=utf-8',
-                    'Authorization' : token
-                },
-                'body': body
-                }, function (err, response) {
-                    if (!err && response.statusCode === 201) {
-                        this.log("Success");
-                    } else {
-                       this.error("Error "+ err );
-                    }
-                    node.status({fill:"red",shape:"ring",text:"waiting"});
-                });
+        this.on('input', function(msg) {
+          eventHubs.init({
+                hubNamespace: config.namespace,
+                hubName: config.hubname,
+                keyName: config.saname,
+                key: config.sakey
             });
-        } else {
-            console.log("Service Bus missing Endpoint");
-        }
+
+            eventHubs.sendMessage({
+                message: msg.payload,
+                deviceId: 1,
+            }).then(function() {
+                console.log('Message Sent!');
+            });
+        });
     }
     RED.nodes.registerType("azure-eventhub", AzureEventHub);
 }
-
